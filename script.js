242a17247ecf22afb4f8581c60cd40bd5048fa0f
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-03 20:12:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-//your JS code here. If required.
-document.querySelector('form').addEventListener('submit', function(event) {
+document.querySelector('#myForm').addEventListener('submit', function(event) {
   event.preventDefault();
 
-  var firstName = document.querySelector('input[name="First Name"]').value;
-  var lastName = document.querySelector('input[name="Last Name"]').value;
-  var phoneNumber = document.querySelector('input[name="Phone Number"]').value;
-  var emailId = document.querySelector('input[name="Email ID"]').value;
+  var firstName = document.querySelector('input[name="firstName"]').value;
+  var lastName = document.querySelector('input[name="lastName"]').value;
+  var phoneNumber = document.querySelector('input[name="phoneNumber"]').value;
+  var emailId = document.querySelector('input[name="emailId"]').value;
 
   alert('First Name: ' + firstName + '\n' +
         'Last Name: ' + lastName + '\n' +
