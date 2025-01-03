fb19625ed8216aaf080a4c0cbbc85be760c659a0
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-03 20:20:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-document.querySelector('#myForm').addEventListener('submit', function(event) {
-  event.preventDefault();
+window.onload = function() {
+  document.querySelector('#myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-  var firstName = document.querySelector('input[name="firstName"]').value;
-  var lastName = document.querySelector('input[name="lastName"]').value;
-  var phoneNumber = document.querySelector('input[name="phoneNumber"]').value;
-  var emailId = document.querySelector('input[name="emailId"]').value;
+    var firstName = document.querySelector('input[name="firstName"]').value;
+    var lastName = document.querySelector('input[name="lastName"]').value;
+    var phoneNumber = document.querySelector('input[name="phoneNumber"]').value;
+    var emailId = document.querySelector('input[name="emailId"]').value;
 
-  alert('First Name: ' + firstName + '\n' +
-        'Last Name: ' + lastName + '\n' +
-        'Phone Number: ' + phoneNumber + '\n' +
-        'Email ID: ' + emailId);
-});
+    alert('First Name: ' + firstName + '\n' +
+          'Last Name: ' + lastName + '\n' +
+          'Phone Number: ' + phoneNumber + '\n' +
+          'Email ID: ' + emailId);
+  });
+};
