fc249c40d10d8cf665add001688708564b43e2fa
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-03 20:06:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var firstName = document.querySelector('input[name="First Name"]').value;
+  var lastName = document.querySelector('input[name="Last Name"]').value;
+  var phoneNumber = document.querySelector('input[name="Phone Number"]').value;
+  var emailId = document.querySelector('input[name="Email ID"]').value;
+
+  alert('First Name: ' + firstName + '\n' +
+        'Last Name: ' + lastName + '\n' +
+        'Phone Number: ' + phoneNumber + '\n' +
+        'Email ID: ' + emailId);
+});
